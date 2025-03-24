08a1ea730b953fea7c8d5330c02994286dbc47d2
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-24 22:16:44 +0300

    define google font and fix ant button primary color

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import { Button } from 'antd';
 function App() {
   return (
    <div>
-    <h1 className="text-3xl font-bold underline text-red-600">helloWorld!</h1>
-    
     <Button type="primary">Primary Button</Button>
+    <h1 className="text-3xl font-bold underline text-red-600">helloWorld!</h1>
    </div>
   );
 }
