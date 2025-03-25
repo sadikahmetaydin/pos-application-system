fb85d68506191bf6fd2af31095f0dbf51770ea2f
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-25 21:22:31 +0300

    create home component structures

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+
+      {/* Home Page */}
+      <div className="home px-6 flex justify-between">
+        {/* Categories */}
+        <div className="categories">
+          <div>categories</div>
+        </div>
+        {/* Products */}
+        <div className="products">
+          <div className="">products</div>
+        </div>
+        {/* Cart Totals */}
+        <div className="">
+          <div>cart totals</div>
+        </div>
+      </div>
+    </>
   );
 }
 
