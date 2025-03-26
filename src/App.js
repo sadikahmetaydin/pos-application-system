dfd2851ae814ba7adb603e0eb1b2b3216912e19d
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-26 20:36:17 +0300

    create products components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
+import Products from "./components/products/Products";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         </div>
         {/* Products */}
         <div className="products flex-[8]">
-          <div className="">products</div>
+          <Products />
         </div>
         {/* Cart Totals */}
         <div className="">
