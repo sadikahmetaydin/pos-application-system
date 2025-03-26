e02b26a91e4de6c09a2b2814710ebdd22b3f74b5
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-26 21:41:25 +0300

    create cart item

--- src/components/cart/CartTotals.jsx
+++ src/components/cart/CartTotals.jsx
@@ -1,14 +1,69 @@
 import { Button } from "antd";
-import { ClearOutlined } from "@ant-design/icons";
+import { ClearOutlined, MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
 
 const CartTotals = () => {
   return (
     <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
       <h2 className="bg-blue-600 text-white text-center py-4 font-bold tracking-wide">Products in Cart</h2>
 
-      <div className="cart-items">
-        <div className="cart-item">cart item</div>
-      </div>
+      <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" 
+            className="w-16 h-16 object-cover pt-2"
+            />
+
+            <div className="flex flex-col ml-2">
+              <b>Apple</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <Button className="w-full flex items-center justify-center !rounded-full" type="primary" size="small" icon={<PlusCircleOutlined />} />
+            <span>1</span>
+            <Button className="w-full flex items-center justify-center !rounded-full" type="primary" size="small" icon={<MinusCircleOutlined />} />
+          </div>
+        </li>
+
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" 
+            className="w-16 h-16 object-cover pt-2"
+            />
+
+            <div className="flex flex-col ml-2">
+              <b>Apple</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <Button className="w-full flex items-center justify-center !rounded-full" type="primary" size="small" icon={<PlusCircleOutlined />} />
+            <span>1</span>
+            <Button className="w-full flex items-center justify-center !rounded-full" type="primary" size="small" icon={<MinusCircleOutlined />} />
+          </div>
+        </li>
+
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" 
+            className="w-16 h-16 object-cover pt-2"
+            />
+
+            <div className="flex flex-col ml-2">
+              <b>Apple</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <Button className="w-full flex items-center justify-center !rounded-full" type="primary" size="small" icon={<PlusCircleOutlined />} />
+            <span>1</span>
+            <Button className="w-full flex items-center justify-center !rounded-full" type="primary" size="small" icon={<MinusCircleOutlined />} />
+          </div>
+        </li>
+      </ul>
 
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
