d6e454aaa921b8a42319b66babd857f412fc87e1
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-29 01:40:47 +0300

    invoice table page responsive

--- src/components/invoices/PrintInvoice.jsx
+++ src/components/invoices/PrintInvoice.jsx
@@ -13,7 +13,7 @@ const PrintInvoices = ({isModalOpen, setIsModalOpen}) => {
               </div>
 
               <div className="invoice-details">
-                <div className="grid grid-cols-4 gap-12">
+                <div className="grid sm:grid-cols-4 grid-cols-3 gap-12">
                   <div className="text-md text-slate-500">
                     <p className="font-bold text-slate-700">Invoice Detail:</p>
                     <p>Unwrapped</p>
@@ -42,10 +42,10 @@ const PrintInvoices = ({isModalOpen, setIsModalOpen}) => {
                     </div>
                   </div>
 
-                  <div className="text-md text-slate-500">
+                  <div className="text-md text-slate-500 sm:block hidden">
                     <div>
                       <p className="font-bold text-slate-700">Terms:</p>
-                      <p>0 Days</p>
+                      <p>10 Days</p>
                     </div>
 
                     <div>
@@ -60,35 +60,41 @@ const PrintInvoices = ({isModalOpen, setIsModalOpen}) => {
                 <table className="min-w-full divide-y divide-slate-500 overflow-hidden">
                   <thead>
                     <tr className="border-b border-slate-200">
-                      <th scope="col" className="py-3.5 pl-4 text-left text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden">Image</th>
-                      <th scope="col" className="py-3.5 pl-4 text-left text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden">Title</th>
-                      <th scope="col" className="py-3.5 pl-4 text-center text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden">Price</th>
-                      <th scope="col" className="py-3.5 pl-4 text-center text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden">Amount</th>
-                      <th scope="col" className="py-3.5 pl-4 text-end text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden">Total</th>
+                      <th scope="col" className="py-3.5 text-left text-sm font-normal text-slate-700 md:pl-0 sm:table-cell hidden">Image</th>
+                      <th scope="col" className="py-3.5 text-left text-sm font-normal text-slate-700 md:pl-0 sm:table-cell hidden">Title</th>
+                      <th colSpan={4} scope="col" className="py-3.5 text-left text-sm font-normal text-slate-700 md:pl-0 sm:hidden">Title</th>
+                      <th scope="col" className="py-3.5 text-center text-sm font-normal text-slate-700 md:pl-0 sm:table-cell hidden">Price</th>
+                      <th scope="col" className="py-3.5 text-center text-sm font-normal text-slate-700 md:pl-0 sm:table-cell hidden">Amount</th>
+                      <th scope="col" className="py-3.5 text-end text-sm font-normal text-slate-700 md:pl-0">Total</th>
                     </tr>
                   </thead>
 
                   <tbody>
                     <tr className="border-b border-t border-slate-200">
-                      <td className="py-4">
+                      <td className="py-4 sm:table-cell hidden">
                         <img src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" 
                           className="w-12 h-12 object-cover"
                         />
                       </td>
 
-                      <td className="py-4">
+                      <td className="py-4 sm:table-cell hidden">
                         <span>Apple</span>
                       </td>
 
-                      <td className="py-4 text-center">
+                      {/* Small Screen */}
+                      <td className="py-4 sm:hidden" colSpan={4}>
+                        <span>Apple</span>
+                      </td>
+
+                      <td className="py-4 text-center sm:table-cell hidden">
                         <span>5₺</span>
                       </td>
 
-                      <td className="py-4 text-center">
+                      <td className="py-4 sm:text-center text-right sm:table-cell hidden">
                         <span>1</span>
                       </td>
 
-                      <td className="py-4  text-end">
+                      <td className="py-4 text-end">
                         <span>5.00₺</span>
                       </td>
                     </tr>
@@ -96,27 +102,45 @@ const PrintInvoices = ({isModalOpen, setIsModalOpen}) => {
 
                   <tfoot>
                     <tr>
-                      <th className="text-right pt-6 pr-12" colSpan="4" scope="row">
+                      <th className="text-right pt-4 pr-12 sm:table-cell hidden" colSpan="4" scope="row">
                         <span className="font-normal text-slate-700">Subtotal</span>
                       </th>
-                      <th className="text-right pt-6"scope="row">
+
+                      {/* Small Screen */}
+                      <th className="text-left pt-4 sm:hidden" scope="row" colSpan="4">
+                        <p className="font-normal text-slate-700">Subtotal</p>
+                      </th>
+
+                      <th className="text-right pt-4" scope="row">
                         <span className="font-normal text-slate-700">36₺</span>
                       </th>
                     </tr>
 
                     <tr>
-                      <th className="text-right pt-3 pr-12" colSpan="4" scope="row">
+                      <th className="text-right pt-4 pr-12 sm:table-cell hidden" colSpan="4" scope="row">
                         <span className="font-normal text-slate-700">Tax</span>
                       </th>
+
+                      {/* Small Screen */}
+                      <th className="text-left pt-4 sm:hidden" scope="row" colSpan="4">
+                        <span className="font-normal text-slate-700">Tax</span>
+                      </th>
+
                       <th className="text-right pt-3"scope="row">
                         <span className="font-normal text-red-600">4.88₺</span>
                       </th>
                     </tr>
 
                     <tr>
-                      <th className="text-right pt-3 pr-12" colSpan="4" scope="row">
+                      <th className="text-right pt-4 pr-12 sm:table-cell hidden" colSpan="4" scope="row">
                         <span className="font-normal text-slate-700">Total</span>
                       </th>
+
+                      {/* Small Screen */}
+                      <th className="text-left pt-4 sm:hidden" scope="row" colSpan="4">
+                        <span className="font-normal text-slate-700">Total</span>
+                      </th>
+
                       <th className="text-right pt-3"scope="row">
                         <span className="font-normal text-slate-700">40.88₺</span>
                       </th>
