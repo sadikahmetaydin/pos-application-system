4ce8cb02871c4e63b4fb8b4681ce4e07b4bfd0c7
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-28 20:59:36 +0300

    create react router dom structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-import CartTotals from "./components/cart/CartTotals";
-import Categories from "./components/categories/Categories";
-import Header from "./components/header/Header";
-import Products from "./components/products/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
     <>
-      <Header />
-
-      {/* Home Page */}
-      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
-        {/* Categories */}
-        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
-          <Categories />
-        </div>
-        {/* Products */}
-        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
-          <Products />
-        </div>
-        {/* Cart Totals */}
-        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
-          <CartTotals />
-        </div>
-      </div>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+     </BrowserRouter>
     </>
   );
 }
