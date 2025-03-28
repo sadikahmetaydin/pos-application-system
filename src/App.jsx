06dedd2f369d2ec2d3ecf8ddfe2d309f26851dea
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-29 01:48:45 +0300

    create customers page table

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import InvoicesPage from "./pages/InvoicesPage";
+import CustomersPage from "./pages/CustomersPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/invoices" element={<InvoicesPage />} />
+        <Route path="/customers" element={<CustomersPage />} />
       </Routes>
      </BrowserRouter>
     </>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
             <span className="md:text-xs text-[10px]">Invoice</span>
           </Link>
 
-          <Link to={"/"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
+          <Link to={"/customers"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Customers</span>
           </Link>
