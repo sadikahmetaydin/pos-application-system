427c54514e5ec08ed974739f8d1a6a3958d643b9
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-04-02 18:34:24 +0300

    mongodb installation

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
 const port = 5000;
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+}
 
 app.get("/", (req, res) => res.send("helloWorld!"));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
+  connect();
 });
