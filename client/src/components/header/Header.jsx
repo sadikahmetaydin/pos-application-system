abcc5bde8957064e3549c4ba1d058dda6caf14ca
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-04-08 20:56:29 +0300

    fix badge and header title bugs

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
             <span className="md:text-xs text-[10px]">Home</span>
           </Link>
 
-          <Badge count={cart.cartItems.length} offset={[0, 6]} className="md:flex hidden">
+          <Badge count={cart.cartItems.length} offset={[0, 0]} className="md:flex hidden">
             <Link to={"/cart"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
                 <ShoppingCartOutlined className="md:text-2xl text-xl" />
                 <span className="md:text-xs text-[10px]">Cart</span>
@@ -60,7 +60,7 @@ const Header = () => {
         </div>
 
         {/* For Responsive Screen */}
-        <Badge count={5} offset={[0, 6]} className="md:hidden flex">
+        <Badge count={5} offset={[0, 0]} className="md:hidden flex">
             <Link to={"/cart"} className="menu-link flex flex-col items-center hover:text-[#40a9ff] transition-all">
                 <ShoppingCartOutlined className="text-2xl" />
                 <span className="md:text-xs text-[10px]">Cart</span>
