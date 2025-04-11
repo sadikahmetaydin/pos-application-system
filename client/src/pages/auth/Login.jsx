f923fb9a506cacc5fec3a57c71e569980b11a378
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-04-11 21:23:30 +0300

    login functionality

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,14 +1,50 @@
-import { Button, Carousel, Checkbox, Form, Input } from "antd";
-import { Link } from "react-router-dom";
+import { Button, Carousel, Checkbox, Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
 import AuthCarousel from "../../components/auth/AuthCarousel";
+import { useState } from "react";
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values) => {
+    try {
+      const res = await fetch("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+
+      const user = await res.json();
+
+      if (res.status === 200) {
+        localStorage.setItem("postUser",
+          JSON.stringify({ username: user.username, email: user.email })
+        );
+        message.success("Login is successfully.");
+        navigate("/");
+      } else if (res.status === 404) {
+        message.error("User not found!")
+      } else if (res.status === 403) {
+        message.error("Invalid Password!")
+      }
+
+      setLoading(false);
+
+    } catch (error) {
+      message.error("Something went wrong!");
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="h-screen">
       <div className="flex justify-between h-full">
         <div className="xl:px-20 px-10 w-full flex flex-col h-full justify-center relative">
           <h1 className="text-center text-5xl font-bold mb-2">LOGO</h1>
-          <Form layout="vertical">
+          <Form layout="vertical" onFinish={onFinish} initialValues={{ remember: false }}>
             
             <Form.Item label="E-mail" name={"email"} rules={[{required: true, message: "Please Enter E-mail!"}]}>
               <Input />
@@ -26,7 +62,7 @@ const Login = () => {
             </Form.Item>
 
             <Form.Item>
-              <Button type="primary" htmlType="submit" className="w-full" size="large">Login</Button>
+              <Button type="primary" htmlType="submit" className="w-full" size="large" loading={loading}>Login</Button>
             </Form.Item>
           </Form>
 
