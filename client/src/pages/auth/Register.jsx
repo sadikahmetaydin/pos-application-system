0f8e5dc9f4c4e39a3c7f430f30c3131d64c778d5
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-04-11 20:52:49 +0300

    register functionality

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,14 +1,41 @@
-import { Button, Carousel, Form, Input } from "antd";
-import { Link } from "react-router-dom";
+import { Button, Carousel, Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
 import AuthCarousel from "../../components/auth/AuthCarousel";
+import { useState } from "react";
 
 const Register = () => {
+
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values) => {
+
+    setLoading(true);
+
+    try {
+      const res = await fetch("http://localhost:5000/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+
+      if (res.status === 200) {
+        message.success("Registration process successful.");
+        navigate("/login");
+        setLoading(false);
+      }
+    } catch (error) {
+      message.error("Something went wrong!");
+      console.log(error);
+    }
+  }
+
   return (
     <div className="h-screen">
       <div className="flex justify-between h-full">
         <div className="xl:px-20 px-10 w-full flex flex-col h-full justify-center relative">
           <h1 className="text-center text-5xl font-bold mb-2">LOGO</h1>
-          <Form layout="vertical">
+          <Form layout="vertical" onFinish={onFinish}>
             <Form.Item label="Username" name={"username"} rules={[{required: true, message: "Please Enter Username!"}]}>
               <Input />
             </Form.Item>
@@ -39,7 +66,7 @@ const Register = () => {
             </Form.Item>
 
             <Form.Item>
-              <Button type="primary" htmlType="submit" className="w-full" size="large">Register</Button>
+              <Button type="primary" htmlType="submit" className="w-full" size="large" loading={loading}>Register</Button>
             </Form.Item>
           </Form>
 
