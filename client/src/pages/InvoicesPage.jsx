3a03ce5ffacca7d914425dffbf984159cece3170
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-04-12 21:22:15 +0300

    invoice page filter features

--- client/src/pages/InvoicesPage.jsx
+++ client/src/pages/InvoicesPage.jsx
@@ -1,13 +1,106 @@
 import Header from "../components/header/Header"
-import { Button, Table } from "antd";
-import { useEffect, useState } from "react";
+import { Button, Input, Space, Table } from "antd";
+import { useEffect, useRef, useState } from "react";
 import PrintInvoice from "../components/invoices/PrintInvoice";
+import { SearchOutlined } from "@ant-design/icons";
+import Highlighter from 'react-highlight-words';
 
 const InvoicesPage = () => {
      
 const [isModalOpen, setIsModalOpen] = useState(false);
 const [invoiceItems, setInvoiceItems] = useState([]);
 const [customer, setCustomer] = useState();
+const [searchText, setSearchText] = useState("");
+const [searchedColumn, setSearchedColumn] = useState("");
+const searchInput = useRef(null);
+
+const handleSearch = (selectedKeys, confirm, dataIndex) => {
+  confirm();
+  setSearchText(selectedKeys[0]);
+  setSearchedColumn(dataIndex);
+};
+
+const handleReset = (clearFilters) => {
+  clearFilters();
+  setSearchText("");
+};
+
+const getColumnSearchProps = dataIndex => ({
+  filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+    <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
+      <Input
+        ref={searchInput}
+        placeholder={`Search ${dataIndex}`}
+        value={selectedKeys[0]}
+        onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+        onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+        style={{ marginBottom: 8, display: 'block' }}
+      />
+      <Space>
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          icon={<SearchOutlined />}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => clearFilters && handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+        <Button
+          type="link"
+          size="small"
+          onClick={() => {
+            confirm({ closeDropdown: false });
+            setSearchText(selectedKeys[0]);
+            setSearchedColumn(dataIndex);
+          }}
+        >
+          Filter
+        </Button>
+        <Button
+          type="link"
+          size="small"
+          onClick={() => {
+            close();
+          }}
+        >
+          close
+        </Button>
+      </Space>
+    </div>
+  ),
+  filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />,
+  onFilter: (value, record) =>
+    record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+  filterDropdownProps: {
+    onOpenChange(open) {
+      if (open) {
+        setTimeout(() => {
+          var _a;
+          return (_a = searchInput.current) === null || _a === void 0 ? void 0 : _a.select();
+        }, 100);
+      }
+    },
+  },
+  render: text =>
+    searchedColumn === dataIndex ? (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[searchText]}
+        autoEscape
+        textToHighlight={text ? text.toString() : ''}
+      />
+    ) : (
+      text
+    ),
+});
 
 useEffect(() => {
   const getInvoices = async () => {
@@ -28,11 +121,13 @@ const columns = [
     title: 'Customer Name',
     dataIndex: 'customerName',
     key: 'customerName',
+    ...getColumnSearchProps("customerName"),
   },
   {
     title: 'Phone Numbers',
     dataIndex: 'customerPhoneNumber',
     key: 'customerPhoneNumber',
+    ...getColumnSearchProps("customerPhoneNumber"),
   },
   {
     title: 'Creation Date',
@@ -46,6 +141,7 @@ const columns = [
     title: "Payment Method",
     dataIndex: "paymentMode",
     key: "paymentMode",
+    ...getColumnSearchProps("paymentMode"),
   },
   {
     title: "Total Price",
@@ -53,7 +149,8 @@ const columns = [
     key: "totalAmount",
     render: (text) => {
       return <span>{text}₺</span>
-    } 
+    },
+    sorter: (a, b) => a.totalAmount - b.totalAmount,
   },
   {
     title: "Actions",
