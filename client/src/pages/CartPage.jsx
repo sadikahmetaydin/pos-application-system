c7e3cda59f233fffd73277791f56ca079b25c7c7
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-04-12 20:57:02 +0300

    cart page table filter features

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,17 +1,109 @@
-import { Button, message, Popconfirm, Table } from "antd";
-import { MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import { Button, Input, message, Popconfirm, Space, Table } from "antd";
+import { MinusCircleOutlined, PlusCircleOutlined, SearchOutlined } from "@ant-design/icons";
 import Header from "../components/header/Header";
 import { Card } from 'antd';
-import { useState } from "react";
+import { useRef, useState } from "react";
 import CreateInvoice from "../components/cart/CreateInvoice";
 import { useDispatch, useSelector } from "react-redux";
 import { increase, decrase, deleteCart } from "../redux/cartSlice";
+import Highlighter from 'react-highlight-words';
 
 const CartPage = () => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+
+  const getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    filterDropdownProps: {
+      onOpenChange(open) {
+        if (open) {
+          setTimeout(() => {
+            var _a;
+            return (_a = searchInput.current) === null || _a === void 0 ? void 0 : _a.select();
+          }, 100);
+        }
+      },
+    },
+    render: text =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
 
   const columns = [
     {
@@ -27,11 +119,13 @@ const CartPage = () => {
       title: 'Product Name',
       dataIndex: 'title',
       key: 'title',
+      ...getColumnSearchProps("title"),
     },
     {
       title: 'Category',
       dataIndex: 'category',
       key: 'category',
+      ...getColumnSearchProps("category"),
     },
     {
       title: 'Product Price',
@@ -41,7 +135,8 @@ const CartPage = () => {
         return (
           <span>{text.toFixed(2)}₺</span>
         )
-      }
+      },
+      sorter: (a, b) => a.price - b.price, 
     },
     {
       title: 'Product Quantity',
