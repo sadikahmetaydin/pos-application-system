742276b2311c7657970aab757f8bfeed087fb9e5
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-04-14 20:22:57 +0300

    delete statistic page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import CustomersPage from "./pages/CustomersPage";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 import ProductPage from "./pages/ProductPage";
-// import StatisticPage from "./pages/StatisticPage";
 import "@ant-design/v5-patch-for-react-19";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
@@ -27,7 +26,6 @@ function App() {
           <Route path="/cart" element={<RouteControl> <CartPage /> </RouteControl>} />
           <Route path="/invoices" element={<RouteControl> <InvoicesPage /> </RouteControl>} />
           <Route path="/customers" element={<RouteControl> <CustomersPage /> </RouteControl>} />
-          {/* <Route path="/statistic" element={<StatisticPage />} /> */}
           <Route path="/products" element={<RouteControl> <ProductPage /> </RouteControl>} />
           <Route path="/register" element={ <Register /> } />
           <Route path="/login" element={ <Login /> } />
